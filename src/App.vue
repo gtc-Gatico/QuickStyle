bc2e7e471b6d3afff246f553ea78c2e597673d8e
tianci.gao <tianci.gao@7x-networks.com>
2023-09-13 13:22:14 +0800

    devui

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { ref } from 'vue'
+import {ref} from 'vue'
 import {RouterLink, RouterView} from 'vue-router'
 import {ThemeServiceInit, infinityTheme, provenceTheme, sweetTheme, deepTheme, galaxyTheme} from 'devui-theme';
 import 'vue-devui/style.css';
+
 const themeList = ref([
   {name: "无限", theme: infinityTheme},
   {name: "蜜糖", theme: sweetTheme},
@@ -10,7 +11,7 @@ const themeList = ref([
   {name: "深邃夜空", theme: deepTheme},
   {name: "追光", theme: galaxyTheme}
 ])
-
+const themeName = ref("")
 const themeService = ThemeServiceInit({infinityTheme}, 'infinityTheme');
 
 const changeTheme = (index) => {
@@ -19,28 +20,49 @@ const changeTheme = (index) => {
 </script>
 
 <template>
-  <d-layout>
+  <d-layout style="height: 100vh;">
     <d-header class="dheader">
-      <d-menu mode="horizontal">
-        <d-menu-item key="home">
-          <template #icon>
-            <i class="icon-homepage"></i>
-          </template>
-          <RouterLink to="/">Home</RouterLink>
-        </d-menu-item>
-        <d-menu-item key="md5">
-          <RouterLink to="/md5">Md5</RouterLink>
-        </d-menu-item>
-        <d-menu-item key="about" route="">
-          <RouterLink to="/about">About</RouterLink>
-        </d-menu-item>
-        <d-menu-item>
-          <d-sub-menu title="主题" key="theme">
-            <d-menu-item v-for="(item, index) in themeList" key="{{item.name}}" @click="changeTheme(index)"> {{item.name}} </d-menu-item>
-          </d-sub-menu>
-        </d-menu-item>
-      </d-menu>
-
+      <d-row>
+        <d-col :span="18">
+          <d-menu mode="horizontal">
+            <d-menu-item key="home">
+              <template #icon>
+                <i class="icon-homepage"></i>
+              </template>
+              <RouterLink to="/">Home</RouterLink>
+            </d-menu-item>
+            <d-menu-item key="md5">
+              <RouterLink to="/md5">Md5</RouterLink>
+            </d-menu-item>
+            <d-menu-item key="about" route="">
+              <RouterLink to="/about">About</RouterLink>
+            </d-menu-item>
+            <!--          <d-menu-item key="theme">-->
+            <!--            <d-sub-menu>-->
+            <!--              <template #icon>-->
+            <!--                <i class="icon-theme-color"></i>-->
+            <!--              </template>-->
+            <!--              <d-menu-item v-for="(item, index) in themeList" key="{{item.name}}" @click="changeTheme(index)">-->
+            <!--                {{ item.name }}-->
+            <!--              </d-menu-item>-->
+            <!--            </d-sub-menu>-->
+            <!--          </d-menu-item>-->
+          </d-menu>
+        </d-col>
+        <d-col :span="6">
+          <d-row justify="end" :span="24" style="background: transparent">
+            <d-menu mode="horizontal" disabled>
+              <d-menu-item key="theme">
+                <d-sub-menu title="主题">
+                  <d-menu-item v-for="(item, index) in themeList" key="{{item.name}}" @click="changeTheme(index)">
+                    {{ item.name }}
+                  </d-menu-item>
+                </d-sub-menu>
+              </d-menu-item>
+            </d-menu>
+          </d-row>
+        </d-col>
+      </d-row>
     </d-header>
     <d-content class="dcontent">
       <RouterView/>
@@ -50,14 +72,15 @@ const changeTheme = (index) => {
 
 <style scoped>
 .dheader, .dfooter {
-  background: #333854;
+  flex: none;
   color: #fff;
+  background: var(--devui-global-bg-normal);
   text-align: center;
   line-height: 40px;
 }
 
 .dcontent {
-  height: 100vh;
+  height: max-content;
   text-align: center;
   background: var(--devui-global-bg-normal)
 }
