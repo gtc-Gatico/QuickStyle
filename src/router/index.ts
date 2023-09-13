37cff8f3f46436c71187163a299f0b9a520f9eae
tianci.gao <tianci.gao@7x-networks.com>
2023-09-13 10:52:43 +0800

    devui

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
@@ -24,12 +24,7 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/Md5.vue')
-    },
-    {
-      path: '*',
-      redirect: '/'
     }
   ]
 })
-
 export default router
