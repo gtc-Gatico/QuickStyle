b2fc477d2240e9950aa512fe7c3084e4e6fc998c
tianci.gao <tianci.gao@7x-networks.com>
2023-09-20 17:58:09 +0800

    websocket

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -15,6 +15,10 @@ function showService() {
   <div class="about">
     <h1>This is an about page</h1>
     <d-button shape="round" size="lg" @click="showService()">666</d-button>
+    <d-textarea>
+      校验IP：Validator.isTrue(StringUtil.isIpv4OrIpv4Segment(haLanBean.getVirtualIp()), CustomerErrorConstants.HA_LAN_VIRTUAL_IP_ILLEGAL, ParameterException.class);
+      校验端口：Validator.isTrue(StringUtil.isPort(cpeConntrackBean.getPort()), CustomerErrorConstants.CONNTRACK_PORT_ILLEGAL, ParameterException.class);
+    </d-textarea>
   </div>
 
 </template>
--- src/views/LocalServer.vue
+++ src/views/LocalServer.vue
@@ -1,29 +1,96 @@
 <template>
   <d-input v-model="cmd"/>
-  <d-button @click="writer" @keyup.enter="writer">发送</d-button>
-  <d-textarea v-model="data" autosize ></d-textarea>
+  <d-button @click="writer" @keyup.enter="writer">发送</d-button><d-button @click="openApp">开启本地服务</d-button><d-button @click="closeApp">关闭本地服务</d-button>
+  <d-textarea id="data" v-model="data" autosize style="max-height: 50vh" @update:modelValue="changeSize($event)"></d-textarea>
 </template>
 
 <script setup>
 import {ref} from 'vue'
-const ws = new WebSocket("ws://localhost:11111/echo")
-console.log(ws)
-ws.onopen = function () {
-  console.log("websocket:open.");
-};
-ws.onmessage = function(res) {
-  data.value = res.data
-}
-ws.onerror = function (res) {
-  console.log("err:" + res);
-}
-const cmd = ref('')
-const data = ref('')
-function writer() {
-  console.log(`{cmd: "${cmd.value}", width: 500}`)
-  ws.send(`{cmd: "${cmd.value}", width: 500}`)
+let ws
+function initWx(){
+  ws = new WebSocket("ws://localhost:80/")
+  ws.onopen = function () {
+    console.log("websocket:open.");
+  };
+  ws.onmessage = function(res) {
+    data.value += res.data
+  }
+  ws.onerror = function (res) {
+    console.log("websocket:error.");
+  }
 }
 
+function openApp(){
+  writerData("hello")
+  window.location.href='icetool://111'
+  initWx()
+}
+function closeApp(){
+  writerData("exit")
+}
+function changeSize(event){
+  if (event) {
+    console.log(event.target)
+    event.target.scrollTop = event.target.scrollHeight
+  }
+
+}
+initWx()
+const cmd = ref('ping baidu.com')
+const data = ref('')
+function writer() {
+  if (ws.readyState == WebSocket.OPEN) {
+    ws.send(cmd.value)
+    console.log("websocket:send.");
+  } else {
+    switch (ws.readyState) {
+      case WebSocket.CONNECTING:
+        // do something
+        console.log("WebSocket正在连接.");
+        break;
+      case WebSocket.OPEN:
+        // do something
+        break;
+      case WebSocket.CLOSING:
+        console.log("WebSocket正在关闭.");
+        break;
+      case WebSocket.CLOSED:
+        // do something
+        console.log("WebSocket已经关闭.");
+      default:
+        // this never happens
+        break;
+    }
+  }
+
+}
+function writerData(data) {
+  if (ws.readyState == WebSocket.OPEN) {
+    ws.send(data)
+    console.log("websocket:send.");
+  } else {
+    switch (ws.readyState) {
+      case WebSocket.CONNECTING:
+        // do something
+        console.log("WebSocket正在连接.");
+        break;
+      case WebSocket.OPEN:
+        // do something
+        break;
+      case WebSocket.CLOSING:
+        console.log("WebSocket正在关闭.");
+        break;
+      case WebSocket.CLOSED:
+        // do something
+        console.log("WebSocket已经关闭.");
+
+      default:
+        // this never happens
+        break;
+    }
+  }
+
+}
 </script>
 
 <style scoped>
